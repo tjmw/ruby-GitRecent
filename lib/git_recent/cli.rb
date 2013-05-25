454f09678e9ba5b0748c2a52269b2b340b949864
Tom Wey <tjmwey@gmail.com>
2013-05-25 20:05:58 +0100

    Refactor duplicate code into method

--- lib/git_recent/cli.rb
+++ lib/git_recent/cli.rb
@@ -3,10 +3,6 @@ module GitRecent
   class Cli < Thor
     desc 'list', 'List recently checked-out git branches'
     def list
-      recent_branch_names = GitRecent::BranchLister.new.branch_names
-
-      abort 'No recent branches' if recent_branch_names.empty?
-
       recent_branch_names.each do |branch_name|
         puts branch_name
       end
@@ -14,16 +10,20 @@ module GitRecent
 
     desc 'checkout', 'Interactively checkout a recently checked-out git branch'
     def checkout
-      recent_branch_names = GitRecent::BranchLister.new.branch_names
-
-      abort 'No recent branches' if recent_branch_names.empty?
-
       chooser = GitRecent::BranchChooser.new recent_branch_names
+
       selected_branch = chooser.request_choice
 
       if selected_branch
         Git.open('.').checkout(selected_branch)
       end
+    end
+
+    private
+    def recent_branch_names
+      recent_branch_names = GitRecent::BranchLister.new.branch_names
+      abort 'No recent branches' if recent_branch_names.empty?
+      recent_branch_names
     end
   end
 end
